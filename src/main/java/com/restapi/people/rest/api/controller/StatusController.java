9bad3b70bf38eb07aa64b9efc77d7c976becfde5
manticorevault <artur_seilah@hotmail.com>
2021-08-16 17:17:45 -0300

    Created the inital setup. Updated the main application with a GET Request.

---
+++ src/main/java/com/restapi/people/rest/api/controller/StatusController.java
@@ -0,0 +1,14 @@
+package com.restapi.people.rest.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping(path = "/api/status")
+    public String check() {
+        return "This application is online!";
+    }
+
+}
---
+++ src/test/java/com/restapi/people/rest/api/JavaRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.people.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
